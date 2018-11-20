cc2e792322b26de987df8bc0025cef00ba1e61e6
Federico Cerutti <CeruttiF@cardiff.ac.uk>
2018-11-20 17:13:14 +0000

    CLI with small example

--- slproblog.py
+++ slproblog.py
@@ -1,10 +1,42 @@
+"""
+Copyright (c) 2018 Federico Cerutti <CeruttiF@cardiff.ac.uk>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
 import sys
+import argparse
 
 from SLProbLog.SLProbLog import SLProbLog
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="Input file")
+    parser.add_argument("-slop", "--subjective-logic-operators", help="Use SL Operators instead of Beta-based", action="store_true")
+
+    args = parser.parse_args()
+
     p = ""
-    with open(sys.argv[1], 'r') as f:
+    with open(args.file, 'r') as f:
         p = f.read()
 
-    print(SLProbLog(p).run_beta())
+    if args.subjective_logic_operators:
+        print(SLProbLog(p).run_SL())
+    else:
+        print(SLProbLog(p).run_beta())
